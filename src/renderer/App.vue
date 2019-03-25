d3f9a85ac2b2c82beb36661946668da9ffa608ea
liuxiang <1753439422@qq.com>
2019-03-25 16:06:52 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'opinion-admin'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
